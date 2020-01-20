f8d3093670b37583fe41aa95ad76e60479df524f
taro.kirameki <taro.kirameki@techacademy.jp>
2020-01-20 08:37:52 +0000

    add colum

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -11,6 +11,12 @@
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
                 <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                
+                <div class="form-group">
                     {!! Form::label('content', 'メッセージ:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
