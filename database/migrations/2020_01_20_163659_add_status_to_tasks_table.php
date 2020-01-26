29bb49ed3256958ed84ba0c0da75c781e2c2ee54
taro.kirameki <taro.kirameki@techacademy.jp>
2020-01-26 13:24:31 +0000

    user login

--- database/migrations/2020_01_20_163659_add_status_to_tasks_table.php
+++ database/migrations/2020_01_20_163659_add_status_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string("status", 10);
+            $table->string("status");
         });
     }
 
@@ -26,7 +26,8 @@ class AddStatusToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string("status", 10);
+            //$table->string("status");
+            $table->dropColumn('status');   //カラムが削除できる機能追加
         });
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,23 @@
                         <a href="{{ route('register') }}">Register</a>
                     @endauth
                 </div>
-            @endif
+           @extends('layouts.app')
+
+@section('content')
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Tasklists</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
+@endsection
+            
+            
+            
 
             <div class="content">
                 <div class="title m-b-md">
